49f2f7aea911342410e7d6a4ab57b09a8f584687
alrog0 <32990779+alrog0@users.noreply.github.com>
2018-05-15 21:56:28 -0500

    Update status_batery_linux.sh

--- status_batery_linux.sh
+++ status_batery_linux.sh
@@ -5,11 +5,11 @@ export XAUTHORITY1=/home/$USER/.Xauthority
 porcentaje_carga_bat=$(cat /sys/class/power_supply/BAT0/capacity)
 fuenteacdc=$(cat /sys/class/power_supply/BAT0/status)
 
-if [ $porcentaje_carga_bat -gt 89 ] && [ $fuenteacdc != "Charging" ] ; then
-    /usr/bin/notify-send --expire-time=45000 -i "battery-full-charged" 'Bateria Cargada!!' "$porcentaje_carga_bat %"
+if [ $porcentaje_carga_bat -gt 89 ] && [ $fuenteacdc = "Charging" ] ; then
+    /usr/bin/notify-send --expire-time=45000 -i "battery-full-charged" 'Bateria Cargada!!!' "$porcentaje_carga_bat%"
 elif
    [ $porcentaje_carga_bat -lt 18 ] && [ $fuenteacdc != "Charging" ] ; then
-    /usr/bin/notify-send --expire-time=45000 -i "battery-caution" 'Bateria Descargada!!' "$porcentaje_carga_bat %"
+    /usr/bin/notify-send --expire-time=45000 -i "battery-caution" 'Bateria Descargada!!!' "$porcentaje_carga_bat%"
 else
 exit 0
 fi
